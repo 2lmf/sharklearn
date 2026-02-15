d146a7d7df8e427f4ebd1133f5234e36f9c0511c
2lmf <2lmf.info@gmail.com>
2026-02-15 20:05:00 +0100

    Service worker v2 - refresh

--- service_worker.js
+++ service_worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sharklearn-v1';
+const CACHE_NAME = 'sharklearn-v2';
 const ASSETS = [
     'index.html',
     'shark_style.css',
@@ -9,9 +9,20 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', event => {
+    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
             return cache.addAll(ASSETS);
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+            );
         })
     );
 });
